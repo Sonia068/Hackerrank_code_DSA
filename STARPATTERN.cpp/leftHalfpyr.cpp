22579b86d9979c6045e119ec317bd0f2b28b3bbd
Sonia068 <soniathakursonia068@gmail.com>
2025-10-14 11:32:06 -0700

    Create leftHalfpyr.cpp

---
+++ STARPATTERN.cpp/leftHalfpyr.cpp
@@ -0,0 +1,20 @@
+// LEFT HALF PYRAMID :
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<2*(n-i)-1;j++){
+            cout<<" ";  
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    
+}
