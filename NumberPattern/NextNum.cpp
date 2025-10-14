a9dd0208e53aa191d6f1963f95d3a7e26a15d47b
Sonia068 <soniathakursonia068@gmail.com>
2025-10-14 11:44:38 -0700

    Create NextNum.cpp

---
+++ NumberPattern/NextNum.cpp
@@ -0,0 +1,22 @@
+// number pattern with next number in triangle :
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    int number =1;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<number;
+            number++;
+        }
+        cout<<"\n";
+    }
+           
+    
+
+    return 0;
+}
