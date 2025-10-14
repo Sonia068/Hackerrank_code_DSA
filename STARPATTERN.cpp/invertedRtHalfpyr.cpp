3b1914def0d9cb2483f7e967a033e8a45a83719c
Sonia068 <soniathakursonia068@gmail.com>
2025-10-14 11:28:43 -0700

    Create invertedRtHalfpyr.cpp

---
+++ STARPATTERN.cpp/invertedRtHalfpyr.cpp
@@ -0,0 +1,18 @@
+// INVERTED RIGHT HALF PYRAMID :
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
